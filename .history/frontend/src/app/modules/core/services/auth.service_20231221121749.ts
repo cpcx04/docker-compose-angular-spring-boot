d652383e6a9b9e6ba0ba3ea7d4f2be3ba76cb14b
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2023-12-21 12:42:59 +0100

    Ejercicio de clase

---
+++ .history/frontend/src/app/modules/core/services/auth.service_20231221121749.ts
@@ -0,0 +1,36 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {CredencialDto} from '../../../shared/shared-models/dto/credencial-dto';
+import {UsuarioDto} from '../../../shared/shared-models/dto/usuario-dto';
+
+//const AUTH_API = 'http://localhost:8080/auth/';
+const AUTH_API = 'http://api:8080/auth/';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  logar(credencialDto: CredencialDto): Observable<any> {
+    return this.http.post(AUTH_API + 'login', {
+      usuario: credencialDto.usuario,
+      senha: credencialDto.senha
+    }, httpOptions);
+  }
+
+  registrar(usuarioDto: UsuarioDto): Observable<any> {
+    return this.http.post(AUTH_API + 'register', {
+      usuario: usuarioDto.usuario,
+      email: usuarioDto.email,
+      senha: usuarioDto.senha
+    }, httpOptions);
+  }
+}
